61509605e492ca4268a2c1efc90bc205bd37f8ac
Justin Tallant <jtallant07@gmail.com>
2013-01-24 03:47:05 -0600

    allow playing against computer

--- lib/game-controller.rb
+++ lib/game-controller.rb
@@ -12,12 +12,10 @@ class Game_Controller
       '7' => '7', '8' => '8', '9' => '9'
     }
 
-    play_game
+    play_game(true)
   end
 
-  def show_board
-
-    puts "Turn Number: #{@turns}"
+  def board_graphic
     board_graphic  = "\n"
     board_graphic += "     |     |     \n"
     board_graphic += "  #{@board["1"]}  |  #{@board["2"]}  |  #{@board["3"]}  \n"
@@ -28,35 +26,155 @@ class Game_Controller
     board_graphic += "     |     |     \n"
     board_graphic += "  #{@board["7"]}  |  #{@board["8"]}  |  #{@board["9"]}  \n"
     board_graphic += "\n"
-    puts board_graphic
+    board_graphic
   end
 
-  def play_game(new_players = true)
+  def play_game(new_game = false)
 
-    welcome if new_players
+    if new_game
+
+      puts "\n Welcome to Tic-Tac-Toe\n"
+
+      puts "Would you like to play against the computer?\n"
+
+      input = gets.chomp.downcase
+
+      if input == 'yes' || input == 'y'
+        # 1 player
+        gather_player_data(1)
+      else
+        # 2 player
+        gather_player_data(2)
+      end
+
+    end
 
     while true
 
-      show_board
+      sleep 1
+      system "clear"
+      puts board_graphic
+      puts "Turn Number: #{@turns}"
+      puts "Your move #{current_player.name}"
 
-      player = current_player
-
-      puts "Your move #{player.name}"
-
-      input = gets.chomp
-
-      case
-      when input == 'q' || input == 'quit' || input == 'exit'
-        break
-      when input.empty? || input.length > 1 || input.match(/[^1-9]/)
-        puts 'invalid input'
-      when @board["#{input}"].match(/[^1-9]/)
-        puts 'That position is already taken'
+      if current_player.computer
+        computer_move
       else
-        update_board(player, input)
+        human_move(gets.chomp.downcase, current_player)
       end
 
       check_for_endgame
+    end
+  end
+
+  # get rid of all the to_s
+  def computer_move
+    if position = victory_at_hand?
+      update_board(@player_two, position.to_s)
+    elsif position = doom_is_near?
+      update_board(@player_two, position.to_s)
+    # create a fork if possible
+    elsif position = fork_available?
+      update_board(@player_two, position.to_s)
+    elsif is_not_taken?(1)
+      update_board(@player_two, '1')
+    elsif is_not_taken?(3)
+      update_board(@player_two, '3')
+    elsif is_not_taken?(7)
+      update_board(@player_two, '7')
+    elsif is_not_taken?(9)
+      update_board(@player_two, '9')
+    elsif is_not_taken?(5)
+      update_board(@player_two, '5')
+    elsif is_not_taken?(2)
+      update_board(@player_two, '2')
+    elsif is_not_taken?(4)
+      update_board(@player_two, '4')
+    elsif is_not_taken?(6)
+      update_board(@player_two, '6')
+    elsif is_not_taken?(8)
+      update_board(@player_two, '8')
+    end
+  end
+
+  def fork_available?
+    false
+  end
+
+  def victory_at_hand?
+
+    finishing_moves.each do |position|
+      if is_owned?(position[0]) && is_owned?(position[1]) && is_not_taken?(position[2])
+        return position[2]
+      end 
+    end
+
+    false
+  end
+
+  def finishing_moves
+    [
+      [1, 2, 3],
+      [1, 3, 2],
+      [1, 4, 7],
+      [1, 5, 9],
+      [1, 7, 4],
+      [1, 9, 5],
+      [2, 3, 1],
+      [2, 5, 7],
+      [2, 8, 5],
+      [3, 5, 7],
+      [3, 6, 9],
+      [3, 7, 5],
+      [3, 9, 6],
+      [4, 5, 6],
+      [4, 7, 1],
+      [5, 6, 4],
+      [5, 7, 3],
+      [5, 9, 1],
+      [6, 9, 3],
+      [7, 8, 9],
+      [7, 9, 8]
+    ]
+  end
+
+  # share that array
+  def doom_is_near?
+
+    finishing_moves.each do |position|
+      if is_owned_by_enemy?(position[0]) && is_owned_by_enemy?(position[1]) && is_not_taken?(position[2])
+        return position[2]
+      end
+    end
+
+    false
+  end
+
+  def is_owned?(position, player = false)
+    player ||= @player_two
+    @board[position.to_s] == player.symbol
+  end
+
+  def is_owned_by_enemy?(position)
+    @board[position.to_s] == @player_one.symbol
+  end
+
+  def is_not_taken?(position)
+    @board[position.to_s] != 'X' && @board[position.to_s] != 'O'
+  end
+
+  def human_move(input, player)
+    case
+    when input == 'q' || input == 'quit' || input == 'exit'
+      Process.exit
+    when input.empty? || input.length > 1 || input.match(/[^1-9]/)
+      puts 'Invalid input! Try again dumbass.'
+      human_move(gets.chomp.downcase, player)
+    when @board["#{input}"].match(/[^1-9]/)
+      puts 'That position is already taken fool.'
+      human_move(gets.chomp.downcase, player)
+    else
+      update_board(player, input)
     end
   end
 
@@ -83,22 +201,19 @@ class Game_Controller
   end
 
   def update_board(player, move_position)
-
     @board[move_position] = player.symbol
     @turns += 1
   end
 
   def check_for_winner(array, player)
-
     if array.uniq == [player.symbol]
       return player
     end
-
     false
   end
 
+  # Doing too much here, try and just return true or false
   def check_for_endgame
-
     winning_sections = [
       [@board['1'], @board['2'], @board['3']],
       [@board['4'], @board['5'], @board['6']],
@@ -115,11 +230,15 @@ class Game_Controller
       winner = check_for_winner(section, player_who_moved_last)
 
       if winner
-        show_board
+        sleep 1
+        system "clear"
+        puts board_graphic
         puts "#{player_who_moved_last.name} WINS!!!"
         ask_to_play_again
       elsif @turns == 10
-        show_board
+        sleep 1
+        system "clear"
+        puts board_graphic
         puts 'CAT game, no possible winner'
         ask_to_play_again
       end
@@ -128,7 +247,6 @@ class Game_Controller
   end
 
   def reset_game
-
     @board = {
       '1' => '1', '2' => '2', '3' => '3',
       '4' => '4', '5' => '5', '6' => '6',
@@ -138,9 +256,10 @@ class Game_Controller
     @turns = 1
   end
 
+  # Doing too much here
   def ask_to_play_again
-
     puts 'Would you like to play again? (yes/no)'
+
     input = gets.chomp
 
     if input == 'yes'
@@ -155,14 +274,16 @@ class Game_Controller
     end
   end
 
-  def welcome
-
-    puts ''
-    puts 'Welcome to Tic-Tac-Toe.'
-    puts ''
-    puts "Enter player one's name:"
-    @player_one = Player.new(gets.chomp, 1, 'X')
-    puts "Enter player two's name:"
-    @player_two = Player.new(gets.chomp, 2, 'O')
+  def gather_player_data(number_players)
+    if 1 == number_players
+      puts "What is your name?"
+      @player_one = Player.new(gets.chomp, 1, 'X', false)
+      @player_two = Player.new('Computer', 2, 'O', true)
+    else
+      puts "Enter player one's name:"
+      @player_one = Player.new(gets.chomp, 1, 'X', false)
+      puts "Enter player two's name:"
+      @player_two = Player.new(gets.chomp, 2, 'O', false)
+    end
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,13 @@
 class Player
-  attr_accessor :name, :id, :symbol
-  def initialize(name, id, symbol)
-    @id = id
+  attr_accessor :name, :id, :symbol, :computer
+  def initialize(name, id, symbol, computer)
+    @id = id # Don't need this
     @name = name
     @symbol = symbol
+    @computer = computer
   end
+
+  # def is_computer?
+  #   @computer
+  # end
 end
